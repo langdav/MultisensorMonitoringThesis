8061e98bf29a5e2c874189dba161381eb3e30fe2
langdav <56968651+langdav@users.noreply.github.com>
2021-07-28 12:16:12 +0200

    Update orthomosaic.R

--- src/orthomosaic.R
+++ src/orthomosaic.R
@@ -5,27 +5,14 @@ library(raster);library(rgeos);library(rgdal); library(sf); library(RStoolbox)
 ## Micasense Channels: Blue, Green, Red, Red Edge, Near Infrared, FLIR (thermal) ##
 ##################################################################################
 # read in 16bit raster (value range 0 - 65535 instead of the value range of 0 - 255 of a 8bit image)
-test <- stack("data/orthomosaic/2021_05_14_orthomosaic_new-2-1.tif")
 test <- stack("data/orthomosaic/2021_04_23_orthomosaic.tif")
-plotRGB(test, r = 3, g = 2, b = 1)
-plot(test$X2021_04_23_orthomosaic.4);plot(las_shp, add=T);plot(single_tree, add=T);plot(single_tree2, add=T)
-
-plot(las_shp);plot(single_tree, add=T)
-
-
-##############################################################
-i <- 2
-las_shp <- sf::read_sf(paste0(paste0("data/single_tree_shapefiles/",trees$tree_id[i],".gpkg"))) #load single tree shapefile)
-plot(las_shp$geom);plot(trees$geometry[i], add = T, col = "red", lwd = 15)
-
-plot(trees$geometry[1],  col = "red", lwd = 15)
-
 
 load("data/trees.RData")
 trees <- sf::st_transform(trees, 25832)
 crs(test) <- crs(trees)
 
-plot(test);plot(trees$geometry, add = T, col = "red", lwd = 15)
+plot(test$X2021_04_23_orthomosaic.1);plot(trees$geometry, add = T, col = "red", lwd = 15)
+
 
 ##############################################################
 
@@ -77,7 +64,7 @@ for(i in 1:length(files)){
 }
 
 ## load tree data 
-load("data/trees_all.RData")
+load("data/trees.RData")
 trees <- sf::st_transform(trees, 25832)
 trees$tree_id <- as.character(trees$tree_id)
 trees <- trees[c(1:50),] #reduce to trees with a budburst record
@@ -104,59 +91,63 @@ for(i in 1:nrow(trees)){
 ## calculate NDVI for single trees using a single BigTIFF ##
 ###########################################################
 ## load tree data 
-load("data/trees_all.RData")
+load("data/trees.RData")
 trees <- st_transform(trees, 25832)
 trees$tree_id <- as.character(trees$tree_id)
 trees <- trees[c(1:50),] #reduce to trees with a budburst record
+trees <- trees[-c(30,37),] #those two trees lay beyond the mosaics boundaries and, hence, need to be excluded
 
+## list available mosaics
+mosaics <- list.files("data/orthomosaic/", pattern = ".tif")
+  
 ## calculate ndvi for all trees
-ndvi_all_trees <- list()
-for(i in 1:nrow(trees)){
-  las_shp <- sf::read_sf(paste0("data/single_tree_shapefiles/",trees$tree_id[19],".gpkg")) #load single tree shapefile
-  if(is.na(crs(las_shp))){
-    sf::st_crs(las_shp) <- 25832
+#ndvi_all_trees <- list()
+ndvi_long <- data.frame(tree_id = NULL, date = NULL, ndvi = NULL)
+ndvi_mean_per_tree <- data.frame(tree_id = NULL, date = NULL, ndvi = NULL)
+
+for(mosaic in mosaics){
+  ortho <- stack(paste0("data/orthomosaic/", mosaic))
+  for(i in 1:nrow(trees)){
+    las_shp <- sf::read_sf(paste0("data/single_tree_shapefiles/",trees$tree_id[i],".gpkg")) #load single tree shapefile
+    # if(is.na(crs(las_shp))){
+    #   sf::st_crs(las_shp) <- 25832
+    # }
+    single_tree <- crop(ortho, las_shp) #crop tile to single tree extent
+    ndvi <- RStoolbox::spectralIndices(single_tree, blue = 1, green = 2, red = 3, nir = 4, indices = "NDVI") #calculate NDVI
+    #ndvi_all_trees[[i]] <- ndvi #write single tree NDVI-rasters into list
+    ndvi <- ndvi@data@values
+    ndvi_long <- rbind(ndvi_long, data.frame(tree_id=rep(trees$tree_id[i], length(ndvi)),
+                                             date=rep(as.Date(substr(mosaic, 1, 10), "%Y_%m_%d"), length(ndvi)),
+                                             ndvi = ndvi))
+    
+    ## remove outliers and calculate NDVI mean values per tree
+    outliers <- which(ndvi %in% c(boxplot.stats(ndvi)$out)) 
+    outliers_removed <- ndvi[-outliers] #remove rows that are containing the outliers
+    
+    if(length(outliers_removed) != 0){
+      ndvi_mean <- mean(outliers_removed)
+      } else {
+      ndvi_mean <- mean(ndvi)
+      }
+    
+    
+    ndvi_mean_per_tree <- rbind(ndvi_mean_per_tree, data.frame(tree_id=trees$tree_id[i],
+                                                               date=as.Date(substr(mosaic, 1, 10), "%Y_%m_%d"),
+                                                               ndvi = ndvi_mean))
+    
   }
-  single_tree <- crop(test, las_shp) #crop tile to single tree extent
-  ndvi <- RStoolbox::spectralIndices(single_tree, blue = 1, green = 2, red = 3, nir = 4, indices = "NDVI") #calculate NDVI
-  ndvi_all_trees[[i]] <- ndvi #write single tree NDVIs into list
 }
 #saveRDS(ndvi_all_trees, paste0("out/orthomosaic/ndvi_per_tree_20210504.rds"))
-#plot(ndvi_all_trees[[24]])
 
-#########################################################################
-## calculate mean NDVI for single trees and match witch budburst data ##
-#######################################################################
-# phenoclasses
+
+## merge with phenoclasses
 phenoclasses <- read.csv("data/budburst_data/budburst_long.csv")
 phenoclasses$date <- as.Date(phenoclasses$date)
 
-ndvi_mean <- data.frame(tree_id = NULL, date = NULL, ndvi = NULL)
-for(i in 1:nrow(trees)){
-  las_shp <- sf::read_sf(paste0("data/single_tree_shapefiles/",trees$tree_id[i],".gpkg")) #load single tree shapefile
-  if(is.na(crs(las_shp))){
-    sf::st_crs(las_shp) <- 25832
-  }
-  single_tree <- crop(test, las_shp) #crop tile to single tree extent
-  ndvi <- RStoolbox::spectralIndices(single_tree, blue = 1, green = 2, red = 3, nir = 4, indices = "NDVI") #calculate NDVI
-  
-  ndvi_mean <- rbind(ndvi_mean, data.frame(tree_id=trees$tree_id[i],
-                                           date=as.Date("2021-04-23"),
-                                           ndvi = mean(ndvi@data@values)))
+ndvi_long_pheno <- merge(ndvi_long, phenoclasses, by = c("tree_id","date"), all.x = T, all.y = F)
+ndvi_mean_pheno <- merge(ndvi_mean_per_tree, phenoclasses, by = c("tree_id","date"), all.x = T, all.y = F)
 
-}
+## save 
+write.csv(ndvi_long_pheno, "out/orthomosaic/ndvi_long_format_phenoclasses_orthomosaic.csv", row.names = F)
+write.csv(ndvi_mean_pheno, "out/orthomosaic/ndvi_mean_per_tree_long_format_phenoclasses_orthomosaic.csv", row.names = F)
 
-ndvi_mean_long <- merge(ndvi_mean, phenoclasses, by = c("tree_id","date"), all.x = T, all.y = F) #merge with phenoclasses
-write.csv(ndvi_mean_long, "out/orthomosaic/ndvi_long_format_phenoclasses_orthomosaic", row.names = F)
-
-
-#############################################
-las_shp <- sf::read_sf(paste0("data/single_tree_shapefiles/",trees$tree_id[17],".gpkg")) #load single tree shapefile
-sf::st_crs(las_shp) <- 25832
-#las_shp <- sf::st_transform(las_shp,  3857)
-#las_shp <- sf::st_transform(las_shp,  "+proj=merc +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0") #reproject to EPSG 42106
-
-single_tree <- crop(test, las_shp) #crop tile to single tree extent
-#ndvi <- RStoolbox::spectralIndices(single_tree, blue = 1, green = 2, red = 3, nir = 4, indices = "NDVI") #calculate NDVI
-
-plotRGB(single_tree, r = 3, g = 2, b = 1)
-plot(ndvi)
