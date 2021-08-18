11a8c6834ecbf19c7f9ed03a1870f21ddbca9e84
langdav <56968651+langdav@users.noreply.github.com>
2021-08-18 15:08:33 +0200

    Update orthomosaic.R

--- src/orthomosaic.R
+++ src/orthomosaic.R
@@ -4,15 +4,13 @@
 #NOTE: Micasense Channels in Order: Blue, Green, Red, Red Edge, Near Infrared, FLIR (thermal)
 
 rm(list = ls())
-library(raster);library(rgeos);library(rgdal); library(sf); library(RStoolbox)
+library(raster);library(rgeos);library(rgdal);library(sf);library(RStoolbox);library(plyr);library(dplyr)
 
 ## list available mosaics
 mosaics <- list.files("data/orthomosaic/", pattern = ".tif")
 
 ## calculate ndvi for all trees
-#ndvi_all_trees <- list()
-ndvi_long <- data.frame(tree_id = NULL, date = NULL, ndvi = NULL)
-ndvi_mean_per_tree <- data.frame(tree_id = NULL, date = NULL, ndvi = NULL)
+ndvi_all <- data.frame(tree_id = NULL, date = NULL, ndvi = NULL)
 
 for(mosaic in mosaics){
   ortho <- stack(paste0("data/orthomosaic/", mosaic))
@@ -24,45 +22,76 @@ for(mosaic in mosaics){
   crs(ortho) <- crs(trees)
   
   for(i in 1:nrow(trees)){
-    las_shp <- sf::read_sf(paste0("data/single_tree_shapefiles/",trees$tree_id[i],".gpkg")) #load single tree shapefile
-    single_tree <- crop(ortho, las_shp) #crop tile to single tree extent
-    ndvi <- RStoolbox::spectralIndices(single_tree, blue = 1, green = 2, red = 3, nir = 4, indices = "NDVI") #calculate NDVI
-    #ndvi_all_trees[[i]] <- ndvi #write single tree NDVI-rasters into list
-    ndvi <- ndvi@data@values
+    cat("Processing", trees_tree_id[i], "in Orthomosaic", mosaic,"\n")
     
+    #load single tree shapefile
+    single_tree_sf <- sf::read_sf(paste0("data/single_tree_shapefiles/",trees$tree_id[i],".gpkg"))
+    
+    #extract green for single tree; do not remove NAs to check if tree lies within the boundaries of the orthomosaic
+    green <- raster::extract(ortho[[2]], single_tree_sf, na.rm = F)
+    
+  
     # as in some mosaics not all trees lay within the extent, trees that contain more than 10 percent NA get removed
-    if(sum(is.na(ndvi)) < 0.1*length(ndvi)){
-      ndvi_long <- rbind(ndvi_long, data.frame(tree_id=rep(trees$tree_id[i], length(ndvi)),
-                                               date=rep(as.Date(substr(mosaic, 1, 10), "%Y_%m_%d"), length(ndvi)),
-                                               ndvi = ndvi))
+    if(sum(is.na(green)) < 0.1*length(green)){
+      #remove NAs from green (if present)
+      green <- na.omit(green)
       
-      ## remove outliers and calculate NDVI mean values per tree
-      outliers <- which(ndvi %in% c(boxplot.stats(ndvi)$out)) 
-      outliers_removed <- ndvi[-outliers] #remove rows that are containing the outliers
+      #extract  red and NIR values for single tree 
+      red <- raster::extract(ortho[[3]], single_tree_sf, na.rm = T)
+      nir <- raster::extract(ortho[[5]], single_tree_sf, na.rm = T)
       
-      if(length(outliers_removed) != 0){
-        ndvi_mean <- mean(outliers_removed)
-      } else {
-        ndvi_mean <- mean(ndvi)
-      }
+      #calculate NDVI = (nir-red)/(nir+red)
+      ndvi <- (nir-red)/(nir+red)
       
+      #compute 80th percentile of "green" values
+      green80thpercentile <- as.numeric(quantile(green, 0.8, na.rm=T))
       
-      ndvi_mean_per_tree <- rbind(ndvi_mean_per_tree, data.frame(tree_id=trees$tree_id[i],
-                                                                 date=as.Date(substr(mosaic, 1, 10), "%Y_%m_%d"),
-                                                                 ndvi = ndvi_mean))
+      #perform brightest pixel masking of NDVI values for reduced background effect; remove NDVI values of cells, where the green band is smaller than the 80th percentile
+      ndvi_80 <- ndvi[which(green>green80thpercentile)]
+      
+      #create output data frame
+      ndvi_all <- rbind(ndvi_all, data.frame(tree_id=rep(trees$tree_id[i], length(ndvi_80)),
+                                               date=rep(as.Date(substr(mosaic, 1, 10), "%Y_%m_%d"), length(ndvi_80)),
+                                               ndvi = ndvi_80))
     }
   }
 }
 
-
-## merge with phenoclasses
+#merge with phenoclasses
 phenoclasses <- read.csv("data/budburst_data/budburst_long.csv")
 phenoclasses$date <- as.Date(phenoclasses$date)
 
-ndvi_long_pheno_orthomosaic <- merge(ndvi_long, phenoclasses, by = c("tree_id","date"), all.x = T, all.y = F)
-ndvi_mean_pheno_orthomosaic <- merge(ndvi_mean_per_tree, phenoclasses, by = c("tree_id","date"), all.x = T, all.y = F)
+ndvi_all_pheno <- merge(ndvi_all, phenoclasses, by = c("tree_id","date"), all.x = T, all.y = F)
 
-## save 
-#write.csv(ndvi_long_pheno, "out/orthomosaic/ndvi_long_format_phenoclasses_orthomosaic.csv", row.names = F) #too big for GitHub; save as RData instead
-save(ndvi_long_pheno_orthomosaic, file = "out/orthomosaic/ndvi_long_format_phenoclasses_orthomosaic.RData")
-save(ndvi_mean_pheno_orthomosaic, file = "out/orthomosaic/ndvi_mean_per_tree_long_format_phenoclasses_orthomosaic.RData")
+#save resulting data frame
+save(ndvi_all_pheno, file = "out/orthomosaic/ndvi_all_with_phenoclasses_orthomosaic.RData")
+
+#remove outliers within each trees single-day-NDVI-values, based on the IQR
+for(tree in unique(ndvi_all_pheno$tree_id)){
+  cat("Processing", tree, "\n")
+  days <- unique(ndvi_all_pheno$date[which(ndvi_all_pheno$tree_id == tree)])
+  for(day in 1:length(days)){
+    if(length(boxplot.stats(ndvi_all_pheno$ndvi[which(ndvi_all_pheno$tree_id == tree &
+                                                   ndvi_all_pheno$date == days[day])])$out) != 0){
+      ndvi_all_pheno <- ndvi_all_pheno[-which(ndvi_all_pheno$tree_id == tree &
+                                          ndvi_all_pheno$date == days[day] &
+                                          ndvi_all_pheno$ndvi %in% c(boxplot.stats(ndvi_all_pheno$ndvi[which(ndvi_all_pheno$tree_id == tree &
+                                                                                                         ndvi_all_pheno$date == days[day])])$out)),]
+    }
+  }
+}
+
+# save resulting data frame
+save(ndvi_all_pheno, file = "out/orthomosaic/outlier_free_ndvi_all_with_phenoclasses_orthomosaic.RData")
+
+# after removing outliers, calculate daily mean NDVI values
+ndvi_per_tree <- ndvi_all_pheno %>% 
+  group_by(tree_id, date) %>% 
+  summarize(ndvi_mean = mean(ndvi, na.rm = T),
+            ndvi_sd = sd(ndvi, na.rm = T),
+            budburst = unique(budburst),
+            budburst_perc = unique(budburst_perc))
+
+# save resulting data frame
+save(ndvi_per_tree, file = "out/orthomosaic/outlier_free_ndvi_mean_per_tree_with_phenoclasses_orthomosaic.RData")
+
