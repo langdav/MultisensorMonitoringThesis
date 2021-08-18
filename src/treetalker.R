a026320bdf417313690aa83f6d93d5f44f51b263
langdav <56968651+langdav@users.noreply.github.com>
2021-08-18 16:21:42 +0200

    Update treetalker.R

--- src/treetalker.R
+++ src/treetalker.R
@@ -27,7 +27,7 @@ tt_data$AS7262_600 <- -328.08+(0.8654*tt_data$AS7262_600)
 tt_data$AS7262_650 <- 202.77+(0.7829*tt_data$AS7262_650)
 
 ## reduce to date range between 15.03. - 01.06.
-tt_data <- tt_data %>% filter(as.Date(timestamp) > as.Date("2021-03-14") & as.Date(timestamp) < as.Date("2021-06-02"))
+#tt_data <- tt_data %>% filter(as.Date(timestamp) > as.Date("2021-03-14") & as.Date(timestamp) < as.Date("2021-06-02"))
 
 ## merge tree ids with tree talker data
 tt_tree_ids <- read.csv("data/TreeTalker/treetalker_tree_ids.csv")
@@ -42,7 +42,7 @@ tt_data <- tt_data[with(tt_data, order(tree_id,date)),]
 tt_pheno <- merge(tt_data, phenoclasses, by = c("tree_id","date"), all.x = F, all.y = F)
 
 ## remove unnecessary rows and rearrange columns
-tt_long_phenoclasses <- subset(tt_pheno,select = -c(record_number, device_type, integration_time, gain))
+tt_long_phenoclasses <- subset(tt_pheno,select = -c(record_number, device_type, integration_time, gain, TT_ID))
 
 tt_long_phenoclasses <- tt_long_phenoclasses %>% relocate(timestamp, .after = date)
 
@@ -51,62 +51,32 @@ tt_long_phenoclasses <- tt_long_phenoclasses[order(tt_long_phenoclasses$tree_id,
 
 tt_long_phenoclasses <- tt_long_phenoclasses[with(tt_long_phenoclasses, order(tree_id,date)),]
 
-## save as csv
-write.csv(tt_pheno, "data/TreeTalker/tt_phenoclasses.csv", row.names = F)
+## save
+save(tt_long_phenoclasses, file = "data/treetalker/tt_phenoclasses_all_spectra.RData")
 
 ## calculate and add NDVI
 #NDVI = (nir-red)/(nir+red); nir = 860 nm, red = 650 nm
 tt_pheno$ndvi <- (tt_pheno$AS7263_860-tt_pheno$AS7262_650)/(tt_pheno$AS7263_860+tt_pheno$AS7262_650)
 
 ndvi_long_format_phenoclasses_treetalker <- subset(tt_pheno, select = c(tree_id, date, timestamp, ndvi, budburst, budburst_perc))
-save(ndvi_long_format_phenoclasses_treetalker, file = "out/treetalker/ndvi_long_format_phenoclasses_treetalker.RData")
 
-##########################################################################################
-##########################################################################################
-##########################################################################################
-##########################################################################################
-## find and remove outliers
-# boxplot for visual detection of outliers
-boxplot(tt_data$AS7263_610)
+## save
+save(ndvi_long_format_phenoclasses_treetalker, file = "out/treetalker/ndvi_all_with_phenoclasses_treetalker.RData")
 
-# which values are the outliers (based on Inter Quantile Range: Q25 - 1.5 * IQR  <-> Q75 + 1.5*IQR; IQR = Q75 - Q25)
-quantile(tt_data$AS7263_610, 0.25) #first quartile
-quantile(tt_data$AS7263_610, 0.75) #third quartile
-IQR(tt_data$AS7263_610) #innerquartile range
-
-boxplot.stats(tt_data$AS7263_610)$out # values wich lay beyond the interquartile  range
-out_ind <- which(tt_data$AS7263_610 %in% c(boxplot.stats(tt_data$AS7263_610)$out)) 
-out_ind #outlier rows
-tt_data$AS7263_610[-out_ind]
-
-# alternative method: find outliers according to 2.5 and 97.5 percentiles
-quantile(tt_data$AS7263_610, 0.025) #lower quantile; all values < this value are outliers
-quantile(tt_data$AS7263_610, 0.975) #upper quantile; all values > this value are outliers
-
-out_ind <- which(tt_data$AS7263_610 < quantile(tt_data$AS7263_610, 0.025) | tt_data$AS7263_610 > quantile(tt_data$AS7263_610, 0.975))
-out_ind #outlier rows
-
-# log - boxplot for visual detection of outliers
-boxplot(log(tt_data$AS7263_610))
-
-# which log-values are the outliers (based on Inter Quantile Range: Q25 - 1.5 * IQR  <-> Q75 + 1.5*IQR; IQR = Q75 - Q25)
-boxplot.stats(log(tt_data$AS7263_610))$out
-
-##################################################
-## histogram with normal distribution curve overlay 
-data <- log(tt_data$AS7263_610)
-std <- sd(data,na.rm=T) #standard deviation of data; for normal distribution
-m <- mean(data,na.rm=T) #mean of data; for normal distribution
-hist(data, freq=F)
-curve(dnorm(x,mean=m,sd=std), add=TRUE,col="red")
-
-## histogram with normal distribution curve overlay for all spectra
-data <- log(tt_data[,5:16])
-std <- lapply(data, sd, na.rm = T)
-m <- lapply(data, mean, na.rm = T)
-
-par(mfrow=c(4,3))
-for(i in 1:12) {
-  hist(data[,i], freq=F, main = paste(colnames(data)[i]))
-  curve(dnorm(x,mean=m[[i]],sd=std[[i]]), add=TRUE,col="red")
+## remove outliers within each trees single-day-NDVI-values, based on the IQR
+for(tree in unique(ndvi_long_format_phenoclasses_treetalker$tree_id)){
+  cat("Processing", tree, "\n")
+  days <- unique(ndvi_long_format_phenoclasses_treetalker$date[which(ndvi_long_format_phenoclasses_treetalker$tree_id == tree)])
+  for(day in 1:length(days)){
+    if(length(boxplot.stats(ndvi_long_format_phenoclasses_treetalker$ndvi[which(ndvi_long_format_phenoclasses_treetalker$tree_id == tree & 
+                                                  ndvi_long_format_phenoclasses_treetalker$date == days[day])])$out) != 0){
+      ndvi_long_format_phenoclasses_treetalker <- ndvi_long_format_phenoclasses_treetalker[-which(ndvi_long_format_phenoclasses_treetalker$tree_id == tree & 
+                                        ndvi_long_format_phenoclasses_treetalker$date == days[day] & 
+                                        ndvi_long_format_phenoclasses_treetalker$ndvi %in% c(boxplot.stats(ndvi_long_format_phenoclasses_treetalker$ndvi[which(ndvi_long_format_phenoclasses_treetalker$tree_id == tree & 
+                                                                                                     ndvi_long_format_phenoclasses_treetalker$date == days[day])])$out)),]
+    }
+  }
 }
+
+## save
+save(ndvi_long_format_phenoclasses_treetalker, file = "out/treetalker/outlier_free_ndvi_mean_per_tree_with_phenoclasses_treetalker.RData")
