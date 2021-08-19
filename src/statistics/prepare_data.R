db2c4ac1e40efe3fc5cc9a427d878d58dd188fa4
langdav <56968651+langdav@users.noreply.github.com>
2021-08-19 18:32:51 +0200

    Update prepare_data.R

--- src/statistics/prepare_data.R
+++ src/statistics/prepare_data.R
@@ -1,26 +1,23 @@
+#author: David Langenohl
+#last modified: 19.08.2021
+#description: create different all-in-one data frames
+#NOTE:
+
 rm(list = ls())
 library(plyr);library(dplyr);library(rstatix);library(ggpubr)
 
-
-#############################################
-## create different all-in-one dataframes ##
-###########################################
-# ----------------------------------------------------------------------------
-## all-in-one daily mean NDVI per tree ---------------------------------------
-
-tt_long_phenoclasses %>% relocate(timestamp, .after = date)
-
-load("out/planetscope/outlier_free_daily_ndvi_mean_per_tree_with_phenoclasses_planetscope.RData");planetscope_mean <- as.data.frame(ungroup(planetscope_mean_per_tree));rm(planetscope_mean_per_tree)
-load("out/sentinel2/ndvi_long_format_phenoclasses_sentinel2.RData");sentinel2_mean <- rename(ndvi_long_pheno_sentinel2, ndvi_mean = ndvi);rm(ndvi_long_pheno_sentinel2);sentinel2_mean$ndvi_sd <- rep(NA, nrow(sentinel2_mean));sentinel2_mean <- sentinel2_mean %>% relocate(ndvi_sd, .after = ndvi_mean)
-load("out/treetalker/outlier_free_ndvi_mean_per_tree_with_phenoclasses_treetalker.RData");treetalker_mean <- as.data.frame(ungroup(treetalker_daily_mean));rm(treetalker_daily_mean)
-load("out/orthomosaic/outlier_free_ndvi_mean_per_tree_with_phenoclasses_orthomosaic.RData");orthomosaic_mean <- as.data.frame(ungroup(ndvi_per_tree));rm(ndvi_per_tree)
+#load data
+load("out/planetscope/outlier_free_daily_ndvi_mean_per_tree_with_phenoclasses_planetscope.RData");planetscope_mean <- as.data.frame(ungroup(planetscope_ndvi_mean_per_tree));rm(planetscope_ndvi_mean_per_tree)
+load("out/sentinel2/ndvi_mean_per_tree_with_phenoclasses_sentinel2.RData");sen2_mean <- as.data.frame(ungroup(sen2_ndvi_mean_per_tree));rm(sen2_ndvi_mean_per_tree)
+load("out/treetalker/outlier_free_ndvi_mean_per_tree_with_phenoclasses_treetalker.RData");treetalker_mean <- as.data.frame(ungroup(treetalker_ndvi_mean_per_tree));rm(treetalker_ndvi_mean_per_tree)
+load("out/orthomosaic/outlier_free_ndvi_mean_per_tree_with_phenoclasses_orthomosaic.RData");orthomosaic_mean <- as.data.frame(ungroup(orthomosaic_ndvi_mean_per_tree));rm(orthomosaic_ndvi_mean_per_tree)
 
 planetscope_mean$platform <- rep("planetscope", nrow(planetscope_mean))
-sentinel2_mean$platform <- rep("sentinel2", nrow(sentinel2_mean))
+sen2_mean$platform <- rep("sentinel2", nrow(sen2_mean))
 treetalker_mean$platform <- rep("treetalker", nrow(treetalker_mean))
 orthomosaic_mean$platform <- rep("orthomosaic", nrow(orthomosaic_mean))
 
-aio_daily_ndvi_means <- rbind(planetscope_mean, sentinel2_mean, treetalker_mean, orthomosaic_mean)
+aio_daily_ndvi_means <- rbind(planetscope_mean, sen2_mean, treetalker_mean, orthomosaic_mean)
 
 # save resulting data frame
 save(aio_daily_ndvi_means, file = "out/all_in_one/aio_daily_ndvi_per_tree_means.RData")
@@ -50,6 +47,7 @@ for(platform in unique(aio$platform)){
 # calculate means over all trees
 aio_all_tree_means <- NULL
 for(platform in unique(aio$platform)){
+  cat("Processing", platform, "\n")
   days <- unique(aio$date[which(aio$platform == platform)])
   for(day in 1:length(days)){
     tmp <- aio[-which(aio$platform == platform & 
@@ -69,4 +67,3 @@ for(platform in unique(aio$platform)){
 # save resulting data frame
 save(aio_all_tree_means, file = "out/all_in_one/aio_all_tree_means_per_platform_and_day.RData")
 
-# =============================================================
