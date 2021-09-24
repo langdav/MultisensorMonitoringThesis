e8b36874fa83bea76e4af72ca117ec72e765092f
langdav <56968651+langdav@users.noreply.github.com>
2021-09-24 13:02:11 +0200

    Update planetscope_treetalker_correlation.R

--- src/statistics/planetscope_treetalker_correlation.R
+++ src/statistics/planetscope_treetalker_correlation.R
@@ -29,13 +29,41 @@ ggplot(aio_mean[which(aio_mean$tree_id %in% unique(aio_mean$tree_id)[1:5]),], ae
   geom_smooth(formula = y ~ x, method = "lm") +
   facet_grid(tree_id~., scales = "free_y")
 
+# testing for correlations
+# Spearman correlation coefficient; insert NAs
+plan <- aio_mean[which(aio_mean$platform=="planetscope"),c("tree_id", "date", "ndvi_mean","platform")]
+tt <- aio_mean[which(aio_mean$platform=="treetalker"),c("tree_id", "date", "ndvi_mean","platform")]
+test <- merge(plan, tt, all = T, by = c("tree_id", "date")) #insert NA for days that have data for one platform but not for the other one
 
+cor.test(test$ndvi_mean.x, test$ndvi_mean.y, method = "spearman") # p < 0.05 -> correlation; rho > 0.5 -> große Effektstärke
+cor.test(test$ndvi_mean.x, test$ndvi_mean.y, method = "spearman", alternative = "greater") # p < 0.05 -> positive correlation
+cor.test(test$ndvi_mean.x, test$ndvi_mean.y, method = "spearman", alternative = "less") # p = 1 -> not a negative correlation
 
+# Spearman correlation coefficient; predict missing data
+library(lubridate)
+plan$date_doy <- lubridate::yday(plan$date)
+plan_mean <- NULL
+for(i in unique(plan$date_doy)){
+  ndvi <- mean(plan$ndvi_mean[which(plan$date_doy == i)], na.rm = T)
+  plan_mean <- rbind(plan_mean, data.frame(ndvi_mean = ndvi,
+                                 date_doy = i))
+}
 
+tt$date_doy <- lubridate::yday(tt$date)
+tt_mean <- NULL
+for(i in unique(tt$date_doy)){
+  ndvi <- mean(plan$ndvi_mean[which(plan$date_doy == i)], na.rm = T)
+  tt_mean <- rbind(tt_mean, data.frame(ndvi_mean = ndvi,
+                                           date_doy = i))
+}
 
+plan_model <- lm(ndvi_mean ~ date_doy, plan_mean)
+tt_model <- lm(ndvi_mean ~ date_doy, tt_mean)
 
-#############################
-## Spearman correlation coefficient
-cor.test(aio_mean$ndvi_mean[which(aio_mean$platform=="planetscope")], aio_mean$ndvi_mean[which(aio_mean$platform=="treetalker")], method = "spearman") # p < 0.05 -> correlation; rho > 0.5 -> große Effektstärke
-cor.test(plan_ndvi$budburst_perc, plan_ndvi$ndvi, method = "spearman", alternative = "greater") # p < 0.05 -> positive correlation
-cor.test(plan_ndvi$budburst_perc, plan_ndvi$ndvi, method = "spearman", alternative = "less") # p = 1 -> not a negative correlation
+dates <- seq(lubridate::yday("2021-03-07"),lubridate::yday("2021-06-01"),1)
+plan_pred <- as.numeric(predict(plan_model,data.frame(date_doy=dates)))
+tt_pred <- as.numeric(predict(tt_model,data.frame(date_doy=dates)))
+
+cor.test(plan_pred, tt_pred, method = "spearman") # p < 0.05 -> correlation; rho > 0.5 -> große Effektstärke
+cor.test(plan_pred, tt_pred, method = "spearman", alternative = "greater") # p < 0.05 -> positive correlation
+cor.test(plan_pred, tt_pred, method = "spearman", alternative = "less") # p = 1 -> not a negative correlation
